76108838cab4b601f70bec5be36348f4ab76e683
wy19981009 <1642837018@qq.com>
2024-06-13 10:43:13 +0800

    空接口 io操作Reader方法

--- learning09/main.go
+++ learning09/main.go
@@ -1,93 +1,162 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
-// DataWriter 定义一个数据写入器
-type DataWriter interface {
-	WriteData(data interface{}) error
-}
+//
+//import (
+//	"fmt"
+//)
+//
+//// DataWriter 定义一个数据写入器
+//type DataWriter interface {
+//	WriteData(data interface{}) error
+//}
+//
+//// 定义文件结构，用于实现DataWriter
+//type file struct {
+//}
+//
+//// WriteData 实现DataWriter接口的WriteData方法
+//func (d *file) WriteData(data interface{}) error {
+//	// 模拟写入数据
+//	fmt.Println("WriteData:", data)
+//	return nil
+//}
+//
+//func main() {
+//	// 实例化file
+//	//f := new(file)
+//	//// 声明一个DataWriter的接口
+//	//var writer DataWriter
+//	//// 将接口赋值f，也就是*file类型
+//	//writer = f
+//	//// 使用DataWriter接口进行数据写入
+//	//err := writer.WriteData("data")
+//	//if err != nil {
+//	//	return
+//	//}
+//	//var x Sayer
+//	//var y Mover
+//	//
+//	//var a = dog{name: "旺财"}
+//	//x = a
+//	//y = a
+//	//x.say()
+//	//y.move()
+//	//var x Mover
+//	//var a = dog{name: "旺财"}
+//	//var b = car{brand: "保时捷"}
+//	//x = a
+//	//x.move()
+//	//x = b
+//	//x.move()
+//	//var x animal
+//	//x = cat{name: "花花"}
+//	//x.move()
+//	//x.say()
+//	// 定义一个空接口x
+//	var x interface{}
+//	s := "ms的go教程"
+//	x = s
+//	fmt.Printf("type:%T value:%v\n", x, x)
+//	i := 100
+//	x = i
+//	fmt.Printf("type:%T value:%v\n", x, x)
+//	b := true
+//	x = b
+//	fmt.Printf("type:%T value:%v\n", x, x)
+//}
+//
+////// Sayer 接口
+////type Sayer interface {
+////	say()
+////}
+////
+////// Mover 接口
+////type Mover interface {
+////	move()
+////}
+////
+////// 接口嵌套
+////type animal interface {
+////	Sayer
+////	Mover
+////}
+////
+////type cat struct {
+////	name string
+////}
+////
+////func (c cat) say() {
+////	fmt.Println("喵喵喵")
+////}
+////
+////func (c cat) move() {
+////	fmt.Println("猫会动")
+////}
 
-// 定义文件结构，用于实现DataWriter
-type file struct {
-}
-
-// WriteData 实现DataWriter接口的WriteData方法
-func (d *file) WriteData(data interface{}) error {
-	// 模拟写入数据
-	fmt.Println("WriteData:", data)
-	return nil
-}
+//func main() {
+//	// 定义一个空接口x
+//	//var x interface{}
+//	//s := "ms的go教程"
+//	//x = s
+//	//fmt.Printf("type:%T value:%v\n", x, x)
+//	//i := 100
+//	//x = i
+//	//fmt.Printf("type:%T value:%v\n", x, x)
+//	//b := true
+//	//x = b
+//	//fmt.Printf("type:%T value:%v\n", x, x)
+//	//
+//	//var studentInfo = make(map[string]interface{})
+//	//studentInfo["name"] = "李白"
+//	//studentInfo["age"] = 18
+//	//studentInfo["married"] = false
+//	//fmt.Println(studentInfo)
+//	var x interface{}
+//	x = "ms的go教程"
+//	v, ok := x.(string)
+//	if ok {
+//		fmt.Println(v)
+//	} else {
+//		fmt.Println("类型断言失败")
+//	}
+//	justifyType(x)
+//}
+//
+//func justifyType(x interface{}) {
+//	switch v := x.(type) {
+//	case string:
+//		fmt.Printf("x is a string，value is %v\n", v)
+//	case int:
+//		fmt.Printf("x is a int is %v\n", v)
+//	case bool:
+//		fmt.Printf("x is a bool is %v\n", v)
+//	default:
+//		fmt.Println("unsupport type！")
+//	}
+//}
 
 func main() {
-	// 实例化file
-	//f := new(file)
-	//// 声明一个DataWriter的接口
-	//var writer DataWriter
-	//// 将接口赋值f，也就是*file类型
-	//writer = f
-	//// 使用DataWriter接口进行数据写入
-	//err := writer.WriteData("data")
-	//if err != nil {
-	//	return
-	//}
-	//var x Sayer
-	//var y Mover
-	//
-	//var a = dog{name: "旺财"}
-	//x = a
-	//y = a
-	//x.say()
-	//y.move()
-	//var x Mover
-	//var a = dog{name: "旺财"}
-	//var b = car{brand: "保时捷"}
-	//x = a
-	//x.move()
-	//x = b
-	//x.move()
-	//var x animal
-	//x = cat{name: "花花"}
-	//x.move()
-	//x.say()
-	// 定义一个空接口x
-	var x interface{}
-	s := "ms的go教程"
-	x = s
-	fmt.Printf("type:%T value:%v\n", x, x)
-	i := 100
-	x = i
-	fmt.Printf("type:%T value:%v\n", x, x)
-	b := true
-	x = b
-	fmt.Printf("type:%T value:%v\n", x, x)
-}
+	reader := strings.NewReader("zhangsan test123 123")
+	// 每次读取4个字节
+	p := make([]byte, 4)
+	for {
 
-//// Sayer 接口
-//type Sayer interface {
-//	say()
-//}
-//
-//// Mover 接口
-//type Mover interface {
-//	move()
-//}
-//
-//// 接口嵌套
-//type animal interface {
-//	Sayer
-//	Mover
-//}
-//
-//type cat struct {
-//	name string
-//}
-//
-//func (c cat) say() {
-//	fmt.Println("喵喵喵")
-//}
-//
-//func (c cat) move() {
-//	fmt.Println("猫会动")
-//}
+		n, err := reader.Read(p)
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("读完了:eof错误 :%d", n)
+				break
+			}
+			log.Printf("其他错误:%v", err)
+			os.Exit(2)
+		}
+		log.Printf("[读取到的字节数为:%d][内容:%v]", n, string(p[:n]))
+	}
+}
--- learning09/类型与接口的关系.go
+++ learning09/类型与接口的关系.go
@@ -1,60 +1,61 @@
 package main
 
-import "fmt"
-
-// Sayer 接口
-type Sayer interface {
-	say()
-}
-
-// Mover 接口
-type Mover interface {
-	move()
-}
-
-type dog struct {
-	name string
-}
-
-type car struct {
-	brand string
-}
-
-// WashingMachine 洗衣机
-type WashingMachine interface {
-	wash()
-	dry()
-}
-
-// 甩干器
-type dryer struct{}
-
-// 海尔洗衣机
-type haier struct {
-	dryer //嵌入甩干器
-}
-
-// 实现Sayer接口
-func (d dog) say() {
-	fmt.Printf("%s会叫汪汪汪\n", d.name)
-}
-
-// 实现Mover接口
-func (d dog) move() {
-	fmt.Printf("%s会动\n", d.name)
-}
-
-// car类型实现Mover接口
-func (c car) move() {
-	fmt.Printf("%s速度70迈\n", c.brand)
-}
-
-// 实现WashingMachine接口的dry()方法
-func (d dryer) dry() {
-	fmt.Println("甩一甩")
-}
-
-// 实现WashingMachine接口的wash()方法
-func (h haier) wash() {
-	fmt.Println("洗刷刷")
-}
+//
+//import "fmt"
+//
+//// Sayer 接口
+//type Sayer interface {
+//	say()
+//}
+//
+//// Mover 接口
+//type Mover interface {
+//	move()
+//}
+//
+//type dog struct {
+//	name string
+//}
+//
+//type car struct {
+//	brand string
+//}
+//
+//// WashingMachine 洗衣机
+//type WashingMachine interface {
+//	wash()
+//	dry()
+//}
+//
+//// 甩干器
+//type dryer struct{}
+//
+//// 海尔洗衣机
+//type haier struct {
+//	dryer //嵌入甩干器
+//}
+//
+//// 实现Sayer接口
+//func (d dog) say() {
+//	fmt.Printf("%s会叫汪汪汪\n", d.name)
+//}
+//
+//// 实现Mover接口
+//func (d dog) move() {
+//	fmt.Printf("%s会动\n", d.name)
+//}
+//
+//// car类型实现Mover接口
+//func (c car) move() {
+//	fmt.Printf("%s速度70迈\n", c.brand)
+//}
+//
+//// 实现WashingMachine接口的dry()方法
+//func (d dryer) dry() {
+//	fmt.Println("甩一甩")
+//}
+//
+//// 实现WashingMachine接口的wash()方法
+//func (h haier) wash() {
+//	fmt.Println("洗刷刷")
+//}
