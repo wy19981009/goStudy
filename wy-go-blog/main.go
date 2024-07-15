80a5a9719a52ed3a53f4f3350acf23e513270f25
wy19981009 <1642837018@qq.com>
2024-07-15 12:21:11 +0800

    首页，页面解析

--- wy-go-blog/main.go
+++ wy-go-blog/main.go
@@ -19,7 +19,16 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t := template.New("index.html")
 	// 拿到当前的路径
 	path, _ := os.Getwd()
-	files, _ := t.ParseFiles(path + "/template/index.html")
+	// 访问博客首页模版的时候需要将涉及的所有模版都进行解析
+	home := path + "/template/home.html"
+	header := path + "/template/layout/header.html"
+	footer := path + "/template/layout/footer.html"
+	personal := path + "/template/layout/personal.html"
+	post := path + "/template/layout/post-list.html"
+	pagination := path + "/template/layout/pagination.html"
+	files, _ := t.ParseFiles(path+"/template/index.html", home, header, footer, personal, post, pagination)
+
+	// 页面上涉及到的所有数据都要有定义
 	files.Execute(w, indexData)
 }
 
